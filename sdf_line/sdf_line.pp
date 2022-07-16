213028d75dd141ab1b0146c9137e0b14bae79187
codecula <109442542+codecula@users.noreply.github.com>
2022-07-17 01:57:09 +0200

    Create sdf_line.pp

---
+++ sdf_line/sdf_line.pp
@@ -0,0 +1,7 @@
+// function sdf_line calculates signed distance of point P from line defined by points A, B
+real sdf_line(pnt2 P, pnt2 A, pnt2 B)
+{
+    vec2 u = B-A;
+    vec2 v = P-A;
+    return (u.y*v.x-u.x*v.y)/length(u);
+}
